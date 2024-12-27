a6edfb2623cd22a2ce834f4ec5520dc89db387bb
amitkatiyar <amit.katiyar@humanitics.com>
2024-12-27 20:39:44 +0530

    Initial commit of Angular build files

---
+++ assets/urlconfig.js
@@ -0,0 +1,11 @@
+ function api_end_point() { return  'http://localhost:3000/api/v1/' }
+ function ai_api_waiting_time(){ return 300000}
+
+
+//  previously used API
+// dont have any information about it
+ function knowledge_graph_api(){ return 'https://hcpoc.humanitics.com/'}
+ function socketpath(){ return 'https://bms.humanitics.com/'}
+
+ function knowledge_graph_search(){return 'http://35.244.5.146:5006/'}
+
